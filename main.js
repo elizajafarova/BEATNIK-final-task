cadb8d50893ba0d144a073c32549f2260913ed47
elizajafarova <eliza.cafarova.f@asoiu.edu.az>
2020-11-23 21:46:35 +0400

    final-task

---
+++ main.js
@@ -0,0 +1,5 @@
+let dropdown = document.getElementById("my-dropdown");
+let dropdownMenu = document.getElementById("my-menu");
+dropdown.addEventListener("mouseenter", function(dropdownMenu){ 
+    dropdownMenu.arget.style.display ="block"
+},500);
